0f3b40dbb554cb9262f88046a7d513e78347c56b
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-12-16 17:47:03 -0500

    no dot

--- src/components/HowToPlay/HowToPlay.jsx
+++ src/components/HowToPlay/HowToPlay.jsx
@@ -4,15 +4,15 @@ const HowToPlay = () => {
     return (
         <>
         <div>
-            <img src='./Screenshots/compile_logo_owned.png' className="headingProtocolDB_rules" alt="Protocol Logo" />
+            <img src='/Screenshots/compile_logo_owned.png' className="headingProtocolDB_rules" alt="Protocol Logo" />
         </div>
         <div className='howToPlayLinks'>
             <a href='https://www.youtube.com/watch?v=eTfG6uVmNSM&ab_channel=GreaterThanGames' target = '_blank'>Great Than Games How To Play Guide!</a>
             <a href='https://www.thefamilygamers.com/compile/' target = '_blank'>The Family Gamers: Video Tutorial and Review</a>
         </div>
         <div className="rule-files">
-        <img src='./Screenshots/rulebookpg1.jpg.png' className="headingProtocolDB" alt="Protocol Database" />
-        <img src='./Screenshots/rulebookpg2.jpg.png' className="headingProtocolDB" alt="Protocol Database" />
+        <img src='/Screenshots/rulebookpg1.jpg.png' className="headingProtocolDB" alt="Protocol Database" />
+        <img src='/Screenshots/rulebookpg2.jpg.png' className="headingProtocolDB" alt="Protocol Database" />
         </div>
         </>
     );
