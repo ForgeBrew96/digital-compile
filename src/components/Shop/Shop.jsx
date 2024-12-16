d0a06c875fde4f22b6fd419892dd4e4c4eef2f7b
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-12-15 21:24:40 -0500

    basic set up for all pages

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,10 +2,15 @@ import './Shop.css'
 
 const Shop = ({ user }) => {
     return (
-        <h1>
-        Shop Page
-        </h1>
+        <div>
+            <div>
+                <img src='protocol_images/Screenshots/compile_logo_owned.png' className="headingProtocolDB_shop" alt="Protocol Logo" />
+            </div>
+            <h1>Buy it now!</h1>
+            <a href='https://www.greaterthangames.com/products/compile-main-1?_pos=9&_sid=1b8067ade&_ss=r' target='_blank' className='shopLink'>Click here for: Greater Than Game's Shop</a>
+            <p>Buy Compile and check out many other great games by Greater Than Game's!</p>
+        </div>
     );
-  };
-  
-  export default Shop;
+};
+
+export default Shop;
